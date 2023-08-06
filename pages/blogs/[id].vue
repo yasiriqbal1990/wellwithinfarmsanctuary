1db580f499206db905c9e953b80104bf42d46f09
Mian Yasir Iqbal <yasiriqbal1990@gmail.com>
2023-08-07 02:13:25 +0400

    Add images to blog cards

--- pages/blogs/[id].vue
+++ pages/blogs/[id].vue
@@ -2,15 +2,12 @@
 
 import useBlogResource from "../../composables/useSingleBlog"
 const {singleBlog, fetchSingleJob} = useBlogResource()
-//@ts-ignore
+
 const title = ref<string>("")
-//@ts-ignore
 const subject = ref<string>("")
-//@ts-ignore
 const body = ref<string>("")
-//@ts-ignore
 const route:any = useRoute()
-//@ts-ignore
+
 onMounted(async()=>{
    
    await fetchSingleJob("blogs",route.params.id)
@@ -29,7 +26,7 @@ onMounted(async()=>{
 
 <template>
     <Header />
-    <article class="flex max-w-xl flex-col items-start justify-between border-gray-100 mx-auto my-auto">
+    <article class="flex max-w-xl mt-20 flex-col items-start justify-between border-gray-100 mx-auto my-auto">
      <h1 class="text-2xl font-bold  ">{{ title }}</h1>
      <h6>{{ subject }}</h6>
      <p v-html="body" class="mt-2 mb-10"></p>
--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -13,8 +13,9 @@ const {blogs} = useBlogResource("blogs")
    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 items-center">
     <div class="m-10  p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-auto overflow-hidden items-center" v-for="blog in blogs" :key="blog.id">
     <a href="#">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ blog.title }}</h5>
+        <h5 class="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">{{ blog.title }}</h5>
     </a>
+    <img src="~/assets/images/qi-bin-w4hbafegiac-unsplash.jpg" alt="Blog Cover Image" >
     <div class="mb-3 font-normal text-gray-700 dark:text-gray-400 "><span class="" v-html="blog.body.split(' ').slice(0, 30).join(' ')"></span><NuxtLink :to="`/blogs/${blog.id}`" class="text-blue-600 underline">Read More...</NuxtLink></div>
 </div>
 </div>
